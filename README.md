bcb630ade91fe0e79a875036e4a1f384a018eed6
Gennady Verdel <godrose@gmail.com>
2018-06-14 09:47:09 +0300

    Added initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # logofx-client-testing-e2e-codedui
 Implementation of start application service and application context using CodedUI
+
+<img src=https://ci.appveyor.com/api/projects/status/github/logofx/logofx-client-testing-e2e-codedui>
---
+++ src/LogoFX.Client.Testing.EndToEnd.CodedUI/ApplicationFacade.cs
@@ -0,0 +1,19 @@
+﻿using LogoFX.Client.Testing.Contracts;
+using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace LogoFX.Client.Testing.EndToEnd.CodedUI
+{
+    class ApplicationFacade : IApplicationFacade
+    {
+        public void Start(string startupPath)
+        {
+            Playback.Initialize();
+            ApplicationUnderTest.Launch(startupPath);
+        }
+
+        public void Stop()
+        {
+            Playback.Cleanup();
+        }
+    }
+}
